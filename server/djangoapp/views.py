b19a009f594ee9a1af4d0a38a48a2fe6959fccea
Andrea Lobo <andrea.lobo@pix4d.com>
2025-06-14 18:23:46 +0200

    chore: more python linting

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 from django.contrib.auth import authenticate, login, logout
+
 # from django.contrib import messages
 # from datetime import datetime
 import logging
@@ -75,13 +76,14 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if request.user.is_anonymous == False:
+    if request.user.is_anonymous is False:
         data = json.loads(request.body)
         try:
             response = post_review(data)
-            return JsonResponse({"status": 200})
-        except:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse({"data": response, "status": 200})
+        except Exception as err:
+            print(f"Unexpected {err=}, {type(err)=}")
+            return JsonResponse({"status": 401, "message": "Error"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
 
@@ -94,5 +96,10 @@ def get_cars(request):
     car_models = CarModel.objects.select_related("car_make")
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append(
+            {
+                "CarModel": car_model.name,
+                "CarMake": car_model.car_make.name,
+            }
+        )
     return JsonResponse({"CarModels": cars})
