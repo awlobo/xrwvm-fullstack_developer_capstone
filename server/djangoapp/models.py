c60e0ee0985b4bed79e714bdfc596338d4f42af1
Andrea Lobo <andrea.lobo@pix4d.com>
2025-06-14 18:10:32 +0200

    chore: lint files

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,10 +1,6 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
@@ -33,28 +29,25 @@ class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        ('HATCHBACK', 'Hatchback'),
-        ('COUPE', 'Coupe'),
-        ('PICKUP', 'Pickup Truck'),
-        ('MINIVAN', 'Minivan'),
-        ('VAN', 'Van'),
-        ('SPORT', 'Sports Car'),
-        ('CROSSOVER', 'Crossover'),
-        ('ELECTRIC', 'Electric'),
-        ('HYBRID', 'Hybrid'),
-        ('LUXURY', 'Luxury'),
-        ('OFFROAD', 'Off-Road'),
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
+        ("HATCHBACK", "Hatchback"),
+        ("COUPE", "Coupe"),
+        ("PICKUP", "Pickup Truck"),
+        ("MINIVAN", "Minivan"),
+        ("VAN", "Van"),
+        ("SPORT", "Sports Car"),
+        ("CROSSOVER", "Crossover"),
+        ("ELECTRIC", "Electric"),
+        ("HYBRID", "Hybrid"),
+        ("LUXURY", "Luxury"),
+        ("OFFROAD", "Off-Road"),
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
+    year = models.IntegerField(
+        default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)]
+    )
 
     def __str__(self):
         return f"{self.name} - {self.type} - {self.year}"
-
