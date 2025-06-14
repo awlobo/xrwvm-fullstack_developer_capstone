0fe22a3229150b1a920fe1743b00f8ac5fbfff63
Andrea Lobo <andrea.lobo@pix4d.com>
2025-06-14 18:03:25 +0200

    fix: fix logout

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,20 +1,19 @@
-# Uncomment the required imports before adding the code
+# from django.shortcuts import render, get_object_or_404, redirect
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
 # from django.contrib import messages
 # from datetime import datetime
-from .restapis import get_request, analyze_review_sentiments, post_review
-from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
+
 import logging
 import json
+
 from django.views.decorators.csrf import csrf_exempt
-from .populate import initiate
 from .models import CarMake, CarModel
+from .populate import initiate
+from .restapis import get_request, analyze_review_sentiments, post_review
+
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -36,9 +35,8 @@ def login_user(request):
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
-    logout(request) # Terminate user session
-    data = {"userName":""} # Return empty username
-    return JsonResponse(data)
+    logout(request)
+    return JsonResponse({"userName": ""})
 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
