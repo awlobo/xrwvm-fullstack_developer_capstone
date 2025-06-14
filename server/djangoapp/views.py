3c24eaa07c2a1a7fb87bb2aeb02afd235d3705a8
Andrea Lobo <andrea.lobo@pix4d.com>
2025-06-14 20:05:33 +0200

    feat: add registration page

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,9 +1,9 @@
 # from django.shortcuts import render, get_object_or_404, redirect
-from django.http import JsonResponse
-from django.contrib.auth import authenticate, login, logout
-
 # from django.contrib import messages
 # from datetime import datetime
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
@@ -38,10 +38,35 @@ def logout_request(request):
     return JsonResponse({"userName": ""})
 
 
-# Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
-# ...
+@csrf_exempt
+def registration(request):
+    data = json.loads(request.body)
+    username = data["userName"]
+    password = data["password"]
+    first_name = data["firstName"]
+    last_name = data["lastName"]
+    email = data["email"]
+    username_exist = False
+    # email_exist = False
+    try:
+        # Check if user already exists
+        User.objects.get(username=username)
+        username_exist = True
+    except User.DoesNotExist:
+        logger.debug(f"{username} is a new user")
+
+    if not username_exist:
+        user = User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=email,
+        )
+        login(request, user)
+        return JsonResponse({"userName": username, "status": "Authenticated"})
+    else:
+        return JsonResponse({"userName": username, "error": "Already Registered"})
 
 
 def get_dealerships(request, state="All"):
